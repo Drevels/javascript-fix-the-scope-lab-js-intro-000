90317eb4f4fe6d7f4210d7e6a875138b643abf02
danielle revels <ddaniellerrevels@aol.com>
2020-06-30 17:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  var two = 2
 
   // Feel free to move things around!
-  const two = 2
+  return n + two;
 }
 
 var funkyFunction = function() {
